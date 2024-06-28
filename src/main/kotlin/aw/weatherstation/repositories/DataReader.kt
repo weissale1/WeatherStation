e5d19bbbcce344d8a1f3efac80c395561c1a8b74
weissale1 <alexander.weiss95@outlook.com>
2024-06-28 11:16:59 +0200

    deleting deprecated classes

--- src/main/kotlin/aw/weatherstation/repositories/DataReader.kt
+++
@@ -1,9 +0,0 @@
-package aw.weatherstation.repositories
-
-import aw.weatherstation.domain.WeatherData
-import org.springframework.stereotype.Component
-
-@Component
-interface DataReader {
-    fun readData(): List<WeatherData>?
-}
