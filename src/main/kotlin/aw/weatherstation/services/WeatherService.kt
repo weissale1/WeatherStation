e4e7b76ed5e8982be3f62c25ad17d2bd65d391bb
weissale1 <alexander.weiss95@outlook.com>
2024-06-28 09:27:09 +0200

    migrate periodic weather recording to new infrastructure

--- src/main/kotlin/aw/weatherstation/services/WeatherService.kt
+++ src/main/kotlin/aw/weatherstation/services/WeatherService.kt
@@ -7,7 +7,10 @@ import kotlinx.coroutines.*
 
 import aw.weatherstation.domain.WeatherData
 import aw.weatherstation.repositories.WeatherRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.reactive.function.client.awaitBody
 
 @Service
@@ -15,6 +18,7 @@ class WeatherService(private val db: WeatherRepository) {
     val webClient = WebClient.builder()
         .baseUrl("http://sensors:5000")
         .build()
+    val logger: Logger = LoggerFactory.getLogger(WeatherService::class.java)
 
     fun getCurrentWeather(): WeatherData {
         val wd: WeatherData? = runBlocking {
@@ -23,8 +27,17 @@ class WeatherService(private val db: WeatherRepository) {
         return wd ?: getLastSavedWeather()
     }
 
-    private fun getLastSavedWeather(): WeatherData {
-        return db.findAll().last()
+    @Scheduled(fixedRate = 300000) // 300,000 milliseconds = 5 minutes
+    fun recordWeatherData() {
+        val wd: WeatherData? = runBlocking {
+            callWeatherSensorService()
+        }
+        if (wd != null) {
+            db.save(wd)
+            logger.info("Recorded WeatherData: ${wd.toString()}")
+        } else {
+            logger.warn("Could not read from WeatherSensors!")
+        }
     }
 
     private suspend fun callWeatherSensorService(): WeatherData? {
@@ -39,4 +52,7 @@ class WeatherService(private val db: WeatherRepository) {
         }
     }
 
+    private fun getLastSavedWeather(): WeatherData {
+        return db.findAll().last()
+    }
 }
