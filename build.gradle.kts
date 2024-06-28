dc905d522023abf067b5b948bdf810f075881235
weissale1 <alexander.weiss95@outlook.com>
2024-06-28 09:48:40 +0200

    add id generation. move to v0.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "aw"
-version = "0.0.3"
+version = "0.0.5"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
--- src/main/kotlin/aw/weatherstation/domain/WeatherData.kt
+++ src/main/kotlin/aw/weatherstation/domain/WeatherData.kt
@@ -1,14 +1,12 @@
 package aw.weatherstation.domain
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "WeatherData")
 data class WeatherData(
-    @Id @JsonIgnore var id: String?,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @JsonIgnore var id: Long? = null,
     val timestamp: String,
     val temp: Float,
     val humid: Float
